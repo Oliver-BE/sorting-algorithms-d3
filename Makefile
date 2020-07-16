846ffc110a1dfcee54f0046a59ee7659b2b1067d
Oliver-BE <obaldwinedwards@gmail.com>
2020-07-16 16:53:52 -0500

    added html layout / favicon, debugged .cc file

--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@ PROJECT := sorting-algorithms-d3
 EMP_DIR := third-party/Empirical/source
 
 # Flags to use regardless of compiler
-CFLAGS_all := -Wall -Wno-unused-function -std=c++17 -I$(EMP_DIR)/
+CFLAGS_all := -Wall -Wno-unused-function -Wno-gnu-zero-variadic-macro-arguments -Wno-dollar-in-identifier-extension -std=c++17 -I$(EMP_DIR)/
+#CFLAGS_all := -Wall -Wno-unused-function -std=c++17 -I$(EMP_DIR)/
 
 # Native compiler information
 CXX_nat := g++
@@ -12,8 +13,9 @@ CFLAGS_nat_debug := -g $(CFLAGS_all)
 
 # Emscripten compiler information
 CXX_web := emcc
-OFLAGS_web_all := -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']" -s TOTAL_MEMORY=67108864 --js-library $(EMP_DIR)/web/library_emp.js --js-library $(EMP_DIR)/web/d3/library_d3.js -s EXPORTED_FUNCTIONS="['_main', '_empCppCallback']" -s DISABLE_EXCEPTION_CATCHING=1 -s NO_EXIT_RUNTIME=1 #--embed-file configs
-OFLAGS_web := -Oz -DNDEBUG
+OFLAGS_web_all := -pedantic -Wno-dollar-in-identifier-extension -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "stringToUTF8"]' -s TOTAL_MEMORY=67108864 --js-library $(EMP_DIR)/web/library_emp.js --js-library $(EMP_DIR)/web/d3/library_d3.js -s EXPORTED_FUNCTIONS="['_main', '_empCppCallback']" -s DISABLE_EXCEPTION_CATCHING=1 -s NO_EXIT_RUNTIME=1 -s WASM=0 #--embed-file configs
+# OFLAGS_web_all := -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']" -s TOTAL_MEMORY=67108864 --js-library $(EMP_DIR)/web/library_emp.js --js-library $(EMP_DIR)/web/d3/library_d3.js -s EXPORTED_FUNCTIONS="['_main', '_empCppCallback']" -s DISABLE_EXCEPTION_CATCHING=1 -s NO_EXIT_RUNTIME=1 #--embed-file configs
+#OFLAGS_web := -Oz -DNDEBUG
 OFLAGS_web_debug := -g4 -Oz -Wno-dollar-in-identifier-extension
 
 CFLAGS_web := $(CFLAGS_all) $(OFLAGS_web) $(OFLAGS_web_all)
--- source/web/sorting-algorithms-d3-web.cc
+++ source/web/sorting-algorithms-d3-web.cc
@@ -2,23 +2,6 @@
 //  Copyright (C) Oliver Baldwin Edwards, 2020.
 //  Released under MIT license; see LICENSE
 
-// #include <iostream>
-
-// #include "web/web.h"
-
-// #include "../example.h"
-
-// namespace UI = emp::web;
-
-// UI::Document doc("emp_base");
-
-// int main()
-// {
-//   doc << "<h1>Hello, browser!</h1>";
-//   std::cout << "Hello, console!" << std::endl;
-//   return example();
-// }
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -26,8 +9,6 @@
 #include "web/init.h"
 #include "web/web.h"
 #include "web/Document.h"
-
-#include "../example.h"
 
 #include "web/js_utils.h"
 #include "base/map.h"
@@ -39,11 +20,92 @@
 #include "web/d3/utils.h"
 
 namespace UI = emp::web;
-UI::Document doc("emp_base");
+UI::Document div_d3viz("emp_d3");
+UI::Document div_d3viz_wrapper("emp_d3_wrapper");
 
-void CreateDivScratch() {
-  // D3::Selection test_select = D3::Select("#emp_d3_test").Append("svg").SetAttr("width", 400).SetAttr("height", 400);
+void CreateD3Viz() {
+
+  EM_ASM({
+    // set the dimensions and margins of the graph
+    var margin = 30;
+    var width = 450;
+    var height = 400;
+
+    var svg = d3.select("#emp_d3")
+                  .append("svg")
+                  .attr("width", width)
+                  .attr("height", height);
+
+    // Create data
+    var data = ([ {x : 10, y : 20}, {x : 40, y : 90}, {x : 80, y : 50} ]);
+
+    // X scale and Axis
+    var x = d3.scaleLinear()
+                .domain([ 0, 100 ])
+                .range([ margin, width - margin ]);
+
+    svg.append('g')
+        .attr("transform", "translate(0," + (height - margin) + ")")
+        .call(d3.axisBottom(x));
+
+    // X scale and Axis
+    var y = d3.scaleLinear()
+                .domain([ 0, 100 ])
+                .range([ height - margin, margin ]);
+
+    svg.append('g')
+        .attr("transform", "translate(" + margin + ",0)")
+        .call(d3.axisLeft(y));
+
+    // Add 3 dots for 0, 50 and 100%
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d){return x(d.x)})
+        .attr("cy", function(d){return y(d.y)})
+        .attr("r", 7);
+
+    console.log("in macro");
+  });
+}
+
+void CreateD3Viz2() {
+
+  int margin = 30;
+  int width = 450;
+  int height = 400;
+
+  D3::Selection svg = D3::Select("#emp_d3_wrapper")
+                          .Append("svg")
+                          .SetAttr("width", width)
+                          .SetAttr("height", height);
   
+  emp::vector<int> data = {10, 100, 300, 234, 53};
+  std::function<int(int, int, int)> return_d =
+      [](int d, int i, int j) { return d; };
+
+  svg.SelectAll("circle")
+      .Data(data)
+      .EnterAppend("circle")
+      .SetAttr("fill", "blue")
+      .SetAttr("cx", 100)
+      .SetAttr("cy", 200)
+      .SetAttr("r", 1)
+      .MakeTransition()
+      .SetDuration(2000)
+      .SetAttr("cx", return_d)
+      .SetAttr("cy", return_d)
+      .SetAttr("r", 7)
+      .SetAttr("fill", "red");
+
+  // EM_ASM({
+  //   $("body").append('<div id="body_emp_test_container"></div>')
+  // });
+  // D3::Selection svg = D3::Select("#emp_base").Append("svg");
+
+  // D3::Selection test_select = D3::Select("#test_test").Append("svg").SetAttr("width", 400).SetAttr("height", 400);
+
   D3::LinearScale test;
   test.SetDomain(0, 1000).SetRange(0, 500);
   int result = test.ApplyScaleInt(500);
@@ -51,11 +113,45 @@ void CreateDivScratch() {
   std::cout << result << std::endl;
 }
 
+void CreateTestDiv() {
+  EM_ASM({
+    $("body").append('<div id="body_emp_test_container"></div>');
+  });
+
+  int margin = 30;
+  int width = 450;
+  int height = 400;
+
+  D3::Selection svg = D3::Select("#body_emp_test_container")
+                          .Append("svg")
+                          .SetAttr("width", width)
+                          .SetAttr("height", height);
+  
+  emp::vector<int> data = {10, 100, 300, 234, 53};
+  std::function<int(int, int, int)> return_d =
+      [](int d, int i, int j) { return d; };
+
+  svg.SelectAll("circle")
+      .Data(data)
+      .EnterAppend("circle")
+      .SetAttr("fill", "blue")
+      .SetAttr("cx", 100)
+      .SetAttr("cy", 200)
+      .SetAttr("r", 1)
+      .MakeTransition()
+      .SetDuration(2000)
+      .SetAttr("cx", return_d)
+      .SetAttr("cy", return_d)
+      .SetAttr("r", 7)
+      .SetAttr("fill", "red");
+}
+
 int main() {
   D3::internal::get_emp_d3();
-  CreateDivScratch();
-  doc << "<h1>Hello, browser!</h1>";
+
+  CreateD3Viz();
+  CreateD3Viz2();
+  CreateTestDiv();
+
   std::cout << "Hello, console!" << std::endl;
-  return example();
-  return 0;
 }
