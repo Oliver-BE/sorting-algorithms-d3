13f8248169f66ab5c4242dccc08df054fee37328
Oliver-BE <obaldwinedwards@gmail.com>
2020-07-15 22:04:45 -0500

    got bare bones functionality working with d3

--- source/web/sorting-algorithms-d3-web.cc
+++ source/web/sorting-algorithms-d3-web.cc
@@ -2,19 +2,60 @@
 //  Copyright (C) Oliver Baldwin Edwards, 2020.
 //  Released under MIT license; see LICENSE
 
-#include <iostream>
+// #include <iostream>
 
+// #include "web/web.h"
+
+// #include "../example.h"
+
+// namespace UI = emp::web;
+
+// UI::Document doc("emp_base");
+
+// int main()
+// {
+//   doc << "<h1>Hello, browser!</h1>";
+//   std::cout << "Hello, console!" << std::endl;
+//   return example();
+// }
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "web/init.h"
 #include "web/web.h"
+#include "web/Document.h"
 
 #include "../example.h"
 
-namespace UI = emp::web;
+#include "web/js_utils.h"
+#include "base/map.h"
 
+#include "web/d3/d3_init.h"
+#include "web/d3/selection.h"
+#include "web/d3/transition.h"
+#include "web/d3/scales.h"
+#include "web/d3/utils.h"
+
+namespace UI = emp::web;
 UI::Document doc("emp_base");
 
-int main()
-{
+void CreateDivScratch() {
+  // D3::Selection test_select = D3::Select("#emp_d3_test").Append("svg").SetAttr("width", 400).SetAttr("height", 400);
+  
+  D3::LinearScale test;
+  test.SetDomain(0, 1000).SetRange(0, 500);
+  int result = test.ApplyScaleInt(500);
+
+  std::cout << result << std::endl;
+}
+
+int main() {
+  D3::internal::get_emp_d3();
+  CreateDivScratch();
   doc << "<h1>Hello, browser!</h1>";
   std::cout << "Hello, console!" << std::endl;
   return example();
+  return 0;
 }
--- tests/example.cc
+++ tests/example.cc
@@ -8,3 +8,39 @@ TEST_CASE("Test example")
 {
   REQUIRE( example() );
 }
+
+// #include "../third-party/Empirical/source/web/Document.h"
+// #include "../third-party/Empirical/source/web/_MochaTestRunner.h"
+
+// struct ExampleTest : emp::web::BaseTest {
+//   int testValue;
+
+//   ExampleTest() { Setup(); }
+
+//   void Setup() {
+//     testValue = 2;
+//   }
+
+//   void Describe() override {
+
+//     EM_ASM({  
+//       const testValue = $0;
+
+//       describe("an example test", function() {
+//         it("should pass in a test value correctly", function() {
+//           chai.assert.equal(testValue, 2); 
+//         });
+//       });
+//     }, testValue);
+//   }
+// };
+
+// emp::web::MochaTestRunner test_runner;
+
+// int main() {
+//   emp::Initialize();
+
+//   test_runner.AddTest<ExampleTest>("ExampleTest");
+  
+//   test_runner.Run();
+// }
